5c394c8b6640023c3569ea078fd8049de0bdcd83
Juno <dghsum@gmail.com>
2022-02-03 02:01:17 +0900

    [Design] css 레이아웃 핫픽스

--- src/Routes/Home/Home.js
+++ src/Routes/Home/Home.js
@@ -15,28 +15,24 @@ function Home() {
   const [firstVisit, setFirstVisit] = useState(true);
 
   useEffect(function () { // 배경화면 줌인 이벤트 리스너
-    var initialScale = 1;
+    var initialScale = 1.2;
     var initialBottom = 0;
-    var initialFontSize = 8;
 
-    const zoomEvent = function (e) { // 첫 랜더링 시 휠 효과
+    const zoomEvent = async function (e) { // 첫 랜더링 시 휠 효과
       var imgElement = document.getElementById('background');
       var wrapper = document.getElementById('wrapper');
       var headerWrapper = document.getElementById('header-wrapper')
-      var title1 = document.getElementById('title1');
-      var title2 = document.getElementById('title2');
 
-      if (e.deltaY < 0 && initialScale > 1) { // 휠 내릴 때
+      if (e.deltaY < 0 && initialScale > 1.2) { // 줌 아웃
+        console.log("working", e.deltaY, '', initialScale)
         headerWrapper.style.opacity = `0`
-        setTimeout(function () { wrapper.style.opacity = `1`; wrapper.style.zIndex = `1` }, 0)
-        initialScale -= 0.1
-        initialBottom += 5
-        
-        imgElement.style.transform = `scale(${(initialScale)})`
-        imgElement.style.bottom = `${initialBottom}px`
+        wrapper.style.opacity = `1`; 
+        wrapper.style.zIndex = `1`
+        imgElement.style.transform = `scale(${(initialScale-= 0.1)})`
+        imgElement.style.bottom = `${initialBottom += 5}px`
 
         
-      } else if (e.deltaY > 0 && initialScale < 2) { // 휠 올릴 때
+      } else if (e.deltaY > 0 && initialScale < 2) { // 줌 인
         wrapper.style.zIndex = `1`
         initialScale += 0.1
         initialBottom -= 5
@@ -48,9 +44,9 @@ function Home() {
 
 
       } else if ( e.deltaY > 0 && initialScale > 2) { // 휠 끝까지 올렸을 때 (이벤트 리스너 제거)
-        wrapper.style.opacity = `0`
-        wrapper.style.zIndex = `-1`
-        setTimeout(function () { headerWrapper.style.opacity = `1` }, 700)
+        wrapper.style.opacity = `0`;
+        wrapper.style.zIndex = `-1`;
+        headerWrapper.style.opacity = `1`;
         setFirstVisit(false)
       }
     }
